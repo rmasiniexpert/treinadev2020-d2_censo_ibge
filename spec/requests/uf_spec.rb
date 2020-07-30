7045fc630b8a0f7d477033abbacd0a0c9b5eb898
Guifs <guifs100@gmail.com>
2020-07-30 14:50:07 -0300

    Exibe o conteúdo do corpo da mensagem

--- spec/requests/uf_spec.rb
+++ spec/requests/uf_spec.rb
@@ -8,7 +8,8 @@ describe Uf do
                  {"id": 5, "sigla": "CO", "nome": "Centro-Oeste"}
                }
               ]}
-  xit 'GET with successfully' do
+  let(:url) {"https://servicodados.ibge.gov.br/api/v1/localidades/estados"}
+  it 'GET with successfully' do
     stub_request(:get, "https://servicodados.ibge.gov.br/api/v1/localidades/estados").
          with(
            headers: {
@@ -18,17 +19,33 @@ describe Uf do
            }).
          to_return(status: 200, body: body.to_json, headers: {})
 
-    response = Faraday.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    response = Faraday.get(url)
     response_body = JSON.parse(response.body, symbolize_names: true)
 
-    # binding.pry
     expect(response.status).to eq 200
     expect(response_body[0][:sigla]).to include('RO')
     expect(response_body[1][:sigla]).to include('DF')
-    expect(Uf.exibe).to include(/Distrito Federal/)
-    expect(Uf.exibe).to include(/DF/)
-    expect(Uf.exibe).to include(/Rondônia/)
-    expect(Uf.exibe).to include(/RO/)
+  end
 
+  it 'Show response body' do
+    stub_request(:get, "https://servicodados.ibge.gov.br/api/v1/localidades/estados").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.0.1'
+           }).
+         to_return(status: 200, body: body.to_json, headers: {})
+
+    response = Faraday.get(url)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq 200
+    expect(response_body[0][:sigla]).to include('RO')
+    expect(response_body[1][:sigla]).to include('DF')
+    expect{Uf.new.exibe(url)}.to output(/Distrito Federal/).to_stdout
+    expect{Uf.new.exibe(url)}.to output(/DF/).to_stdout
+    expect{Uf.new.exibe(url)}.to output(/Rondônia/).to_stdout
+    expect{Uf.new.exibe(url)}.to output(/RO/).to_stdout
   end
 end
