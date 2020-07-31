ac0e7c1c1b681a0e86cd6ffb77a30bed4581bc43
Guifs <guifs100@gmail.com>
2020-07-31 12:34:50 -0300

    iniciando consulta 2

--- lib/city.rb
+++ lib/city.rb
@@ -1,4 +1,49 @@
+require 'faraday'
+require 'json'
+require 'terminal-table'
 class City
-  
-  
+
+  def select_city_id(url)
+    uf_id = []
+    response = Faraday.get(url)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    table = show_ufs(response_body)
+    loop do
+      uf_id = select_Uf(response_body)
+      unless uf_id[1].nil?
+        break
+      end
+      puts "\n=================================================="
+      puts "\n\nValor inválido, digite uma UF da tabela\n\n"
+      puts table
+    end
+    puts "\n==================================================\n"
+    puts "\n\nBuscando Tabelas de ranking dos nomes comuns em #{uf_id[0]}"
+    uf_id[1]
+  end
+
+  def show_ufs(response_body)
+    rows = []
+    headings = []
+    headings <<  ['UF', 'Nome do Estado']
+    response_body.each do |uf|
+      rows << [uf[:sigla], uf[:nome]]
+    end
+    table = Terminal::Table.new(rows: rows, headings: headings)
+    puts table
+    table
+  end
+
+  def select_City(response_body)
+    aux = []
+    print "Digite a UF que deseja buscar os nomes comuns: "
+    input = gets.upcase.chomp
+    response_body.each do |uf|
+      if uf[:sigla] == input
+        aux << input
+        aux << uf[:id]
+      end
+    end
+    aux
+  end
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe City do
-  
+  it 'successfully' do
+    
+  end
 end
